410f2ac93f179c401136328cb530bf1739b2344d
Dan Lawson <dan@pinchpoint.com.au>
2020-03-15 19:00:06 +1100

    💸

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,8 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://danlsn-com-au.netlify.com/`,
     social: {
-      instagram: `dan.lsn`,
+      twitter: `_danlsn`,
+      instagram: `dan.lsn`
     },
   },
   plugins: [
